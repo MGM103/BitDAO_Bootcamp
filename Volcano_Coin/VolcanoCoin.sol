d7b8b45b0fc631347977e3796db4b7c6ff44b57a
Marcus Marinelli <marcus.marinelli3@gmail.com>
2021-11-10 21:07:51 +1100

    Create VolcanoCoin.sol
    Homework 3

---
+++ Volcano_Coin/VolcanoCoin.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract VolcanoCoin {
+    uint totalSupply = 1000;
+    address owner;
+    
+    event totalSupplyChange(uint);
+    
+    modifier isOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor(){
+        owner = msg.sender;
+    }
+    
+    function getTotalSupply() public view returns(uint){
+        return totalSupply;
+    }
+    
+    function increaseTotalSupply() public isOwner {
+        totalSupply = totalSupply + 1000;
+        emit totalSupplyChange(totalSupply);
+    }
+}
