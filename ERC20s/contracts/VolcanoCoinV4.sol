60160c6a73388121fecb599ec1a1593dde2938fa
Marcus Marinelli <marcus.marinelli3@gmail.com>
2022-01-05 22:29:24 +1100

    Beginning homework 12

---
+++ ERC20s/contracts/VolcanoCoinV4.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract VolcanoCoinV4 is ERC20("Volcano Coin", "VLC"), Ownable {
+
+    uint256 constant versionNum = 1;
+    uint256  constant initialSupply = 100000;
+    address admin;
+    
+    using Counters for Counters.Counter;
+    Counters.Counter private _paymentIDs;
+
+    using Strings for uint256;
+
+    struct Payment{
+        uint256 paymentID;
+        PaymentTypes paymentType;
+        uint256 amount;
+        address recipient;
+        uint256 timestamp;
+        string comment;
+    }
+
+    enum PaymentTypes {Unkown, BasicPayment, Refund, Dividend, GroupPayment}
+    PaymentTypes constant defaultChoice = PaymentTypes.Unkown;
+
+    mapping (address => Payment[]) public payments;
+    event supplyChanged(uint256);
+
+    modifier isAdmin(){
+        require(msg.sender == admin);
+        _;
+    }
+
+    constructor() {
+        _mint(msg.sender, initialSupply);
+        admin = msg.sender;
+    }
+
+    function transfer(address _recipient, uint _amount) public virtual override returns (bool) {
+        _transfer(msg.sender, _recipient, _amount);
+        addPaymentRecord(msg.sender, _recipient, _amount);
+        return true;
+    }
+
+    function addPaymentRecord(address _sender, address _recipient, uint _amount) internal {
+        payments[_sender].push(
+            Payment(_paymentIDs.current(), PaymentTypes.Unkown, _amount,_recipient, block.timestamp, "")
+        );
+        _paymentIDs.increment();
+    }
+
+    function addToTotalSupply(uint256 _quantity) public onlyOwner {
+        _mint(msg.sender,_quantity);
+        emit supplyChanged(_quantity);
+    }
+
+    function getPayments(address _user) public view returns (Payment[] memory) {
+        return payments[_user];
+    }
+
+    function viewPayments(address _user) public view returns (string memory) {
+        string memory paymentInfo = "";
+        string memory currentInfo;
+        Payment[] memory userPayments = payments[_user];
+
+        for(uint256 i = 0; i < userPayments.length; i++){
+            currentInfo = string(abi.encodePacked(
+                Strings.toString(userPayments[i].paymentID), "\t", 
+                paymentType2String(userPayments[i].paymentType), "\t", 
+                Strings.toString(userPayments[i].amount), "\t", 
+                Strings.toString(userPayments[i].timestamp), "\t", 
+                "0x", toAsciiString(userPayments[i].recipient), "\t", 
+                userPayments[i].comment, "\n"
+            ));
+            paymentInfo = string(abi.encodePacked(paymentInfo, currentInfo));
+        }
+
+        return paymentInfo;
+    }
+
+    //Assumes that the user that made a payment and the admin are the only ones able to update it
+    function updatePayment(uint256 _payID, PaymentTypes _paymentType, string calldata _comment) public {
+        //Not sure about require for enum
+        //require(_comment); not sure about verifying string either
+
+        for(uint256 i = 0; i < payments[msg.sender].length; i++){
+            if(payments[msg.sender][i].paymentID == _payID){
+                payments[msg.sender][i].paymentType = _paymentType;
+                payments[msg.sender][i].comment = _comment;
+            }
+        }
+    }
+
+    function adminPaymentUpdate(address _user, uint256 _payID, PaymentTypes _paymentType) public isAdmin {
+        require(_user != address(0), "Invalid address specified");
+        //Not sure about require for enum
+        //require(_comment); not sure about verifying string either
+
+        for(uint256 i = 0; i < payments[_user].length; i++){
+            if(payments[_user][i].paymentID == _payID){
+                payments[_user][i].paymentType = _paymentType;
+                payments[_user][i].comment = string(abi.encodePacked(
+                    payments[_user][i].comment,
+                    " updated by: 0x", 
+                    toAsciiString(msg.sender)
+                ));
+            }
+        }
+    }
+
+    function getVersionNum() public view returns(uint256){
+        return versionNum;
+    }
+
+    function paymentType2String(PaymentTypes _paymentType) internal pure returns(string memory) {
+        string memory retVal;
+
+        if(_paymentType == PaymentTypes.Unkown){
+            retVal = "Unkown";
+        }else if(_paymentType == PaymentTypes.BasicPayment){
+            retVal = "Basic Payment";
+        }else if(_paymentType == PaymentTypes.Refund){
+            retVal = "Refund";
+        }else if(_paymentType == PaymentTypes.Dividend){
+            retVal = "Dividend";
+        }else{
+            retVal = "Group Payment";
+        }
+
+        return retVal;
+    }
+
+    //The following two functions were borrowed for address to string conversion from eth stack exchange
+    function toAsciiString(address _x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(_x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);            
+        }
+        return string(s);
+    }
+
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
+    }
+}
---
+++ ERC20s/test/testVlcV4.js
@@ -0,0 +1,184 @@
+const { expect, use } = require("chai");
+const { ethers } = require("hardhat");
+const {
+  constants, // Common constants, like the zero address and largest integers
+  expectRevert, // Assertions for transactions that should fail
+} = require("@openzeppelin/test-helpers");
+
+const { solidity } = require("ethereum-waffle");
+const expectEvent = require("@openzeppelin/test-helpers/src/expectEvent");
+use(solidity);
+
+describe("Volcano Coin V4",  () => {
+  let volcanoContract;
+  let owner, user1, user2, user3;
+
+  beforeEach(async () => {
+    const volcanoCoin = await ethers.getContractFactory("VolcanoCoinV4");
+    
+    volcanoContract = await volcanoCoin.deploy();
+    await volcanoContract.deployed();
+
+    [owner, user1, user2, user3] = await ethers.getSigners();
+  });
+
+  it("Returns the correct version number on deployment", async () => {
+    let versionNum = 1;
+    let contractVersion = await volcanoContract.getVersionNum();
+    expect(contractVersion).to.equal(versionNum);
+  })
+
+  it("assigns initial balance to owner", async () => {
+    let ownerBal = await volcanoContract.balanceOf(owner.address);
+    expect(ownerBal).to.equal(100000);
+  });
+
+  it("Updates balances on successful transfer between owner and user1", async () => {
+    let transferAmount = 25000;
+    let balanceOwnerBefore = await volcanoContract.balanceOf(owner.address);
+    let balanceUser1Before = await volcanoContract.balanceOf(user1.address);
+
+    let transferTx = await volcanoContract.transfer(user1.address, transferAmount);
+    await transferTx.wait();
+
+    let balanceOwnerAfter = await volcanoContract.balanceOf(owner.address);
+    let balanceUser1After = await volcanoContract.balanceOf(user1.address);
+
+    expect(balanceOwnerBefore - balanceOwnerAfter).to.equal(transferAmount);
+    expect(balanceUser1After - balanceUser1Before).to.equal(transferAmount);
+  });
+
+  it("Creates a payment record on transfer", async () => {
+    let transferAmount = 1000;
+
+    let transferTx = await volcanoContract.transfer(user1.address, transferAmount);
+    await transferTx.wait();
+
+    let payments = await volcanoContract.getPayments(owner.address);
+    //console.log("Payments made by the owner:", payments);
+    //console.log("Transaction ID:", parseInt(ethers.utils.formatEther(payments[0].paymentID)));
+
+    expect(payments.length).to.not.equal(0);
+    expect(parseInt(ethers.utils.formatEther(payments[0].paymentID))).to.equal(0);
+    expect(payments[0].paymentType).to.equal(0);
+    expect(payments[0].recipient).to.equal(user1.address);
+    expect(parseInt(ethers.utils.formatEther(payments[0].amount)));
+    expect(payments[0].comment).to.equal("");
+  });
+
+  it("Increases total supply correctly when called", async () => {
+    let supplyIncrease = 50000;
+    let originalSupply = await volcanoContract.totalSupply();
+
+    let supplyTx = await volcanoContract.addToTotalSupply(supplyIncrease);
+    await supplyTx.wait();
+
+    let newSupply = await volcanoContract.totalSupply();
+
+    expect(newSupply - originalSupply).to.equal(supplyIncrease);
+  });
+
+  it("Emits an event when total supply is increased", async () => {
+    let supplyTx = await volcanoContract.addToTotalSupply(10000);
+    await supplyTx.wait();
+    expect(supplyTx).to.emit(volcanoContract, 'supplyChanged');
+  });
+
+  it("Returns a formatted string with full payment history", async () => {
+    let transferAmount = 10000;
+    let defaultTransactionType = "Unkown";
+    let time1;
+    let time2;
+    let comment = '';
+
+    let blockNum1 = await ethers.provider.getBlockNumber();
+    time1 = await ethers.provider.getBlock(blockNum1);
+    time1 = time1.timestamp + 1;
+
+    let transferTx = await volcanoContract.transfer(user1.address, transferAmount);
+    await transferTx.wait();
+
+    let blockNum2 = await ethers.provider.getBlockNumber();
+    time2 = await ethers.provider.getBlock(blockNum2);
+    time2 = time2.timestamp + 1;
+
+    transferTx = await volcanoContract.transfer(user2.address, transferAmount);
+    await transferTx.wait();
+
+    let paymentHist = await volcanoContract.viewPayments(owner.address);
+    // console.log(`Timestamp one is ${time1} and timestamp two is ${time2}`);
+    // console.log(paymentHist);
+
+    let answerString = `0\t${defaultTransactionType}\t${transferAmount}\t${time1}\t${user1.address.toLowerCase()}\t${comment}\n1\t${defaultTransactionType}\t${transferAmount}\t${time2}\t${user2.address.toLowerCase()}\t${comment}\n`;
+    expect(paymentHist).to.equal(answerString);
+  })
+
+  it("Allows the user to update relevant fields of a payment", async ()=> {
+    let transferAmount = 10000;
+    let comment = "Alpha Leak Payment";
+    let newPayType = 1;
+
+    let transferTx = await volcanoContract.transfer(user1.address, transferAmount);
+    await transferTx.wait();
+
+    let updateTx = await volcanoContract.updatePayment(0, newPayType, comment);
+    await updateTx.wait();
+
+    let transacHist = await volcanoContract.getPayments(owner.address);
+    let transac1 = transacHist[0];
+
+    expect(transac1.paymentType).to.equal(newPayType);
+    expect(transac1.comment).to.equal(comment);
+  })
+
+  it("Reverts when given a negative payID to update", async () => {
+    let transferAmount = 10000;
+    let comment = "Alpha Leak Payment";
+    let newPayType = 1;
+
+    let transferTx = await volcanoContract.transfer(user1.address, transferAmount);
+    await transferTx.wait();
+
+    await expectRevert(
+      volcanoContract.updatePayment(-1, newPayType, comment),
+      "value out-of-bounds"
+    )
+  })
+
+  it("Allows admin to update a payment", async () => {
+    let transferAmount = 10000;
+    let newPayType = 3;
+    let payID = 1;
+    let comment = ` updated by: ${owner.address}`;
+
+    let transferTx = await volcanoContract.transfer(user1.address, transferAmount);
+    await transferTx.wait();
+    transferTx = await volcanoContract.connect(user1).transfer(user2.address, transferAmount);
+    await transferTx.wait();
+
+    let updateTx = await volcanoContract.adminPaymentUpdate(user1.address, payID, newPayType);
+    await updateTx.wait();
+
+    let paymentHist = await volcanoContract.getPayments(user1.address);
+    let payment1 = paymentHist[0];
+
+    expect(payment1.paymentType).to.equal(newPayType);
+    expect(payment1.comment).to.equal(comment.toLowerCase());
+  })
+
+  it("Reverts when admin provides 0 address for payment update", async () => {
+    let transferAmount = 10000;
+    let newPayType = 3;
+    let payID = 1;
+
+    let transferTx = await volcanoContract.transfer(user1.address, transferAmount);
+    await transferTx.wait();
+    transferTx = await volcanoContract.connect(user1).transfer(user2.address, transferAmount);
+    await transferTx.wait();
+
+    await expectRevert(
+      volcanoContract.adminPaymentUpdate(constants.ZERO_ADDRESS, payID, newPayType),
+      "Invalid address specified"
+    )
+  })
+});
